ca52ffd7c075c76166dfead5028668b2595af04e
Wario <37343942+IfreannMedia@users.noreply.github.com>
2021-10-23 14:50:45 +0200

    Initial commit

---
+++ src/test/java/com/ifreann/spring/rest/simplerestcontroller/SimpleRestControllerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ifreann.spring.rest.simplerestcontroller;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SimpleRestControllerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
