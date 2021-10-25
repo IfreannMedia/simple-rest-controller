bd5cc9cc50a183c50f34db6a78ae4333d80464e5
Wario <37343942+IfreannMedia@users.noreply.github.com>
2021-10-25 22:15:40 +0200

    set up a live reload
    looked into debug logging of the application server

--- src/main/java/com/ifreann/spring/rest/simplerestcontroller/BooksController.java
+++ src/main/java/com/ifreann/spring/rest/simplerestcontroller/BooksController.java
@@ -3,7 +3,6 @@ package com.ifreann.spring.rest.simplerestcontroller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.util.Arrays;
 import java.util.List;
 
 @RestController
@@ -11,6 +10,6 @@ public class BooksController {
 
     @GetMapping("/books")
     public List<Book> getAllBooks() {
-        return List.of(new Book(1, "How to Win Friends and Influence People", "Dale Carnegie"));
+        return List.of(new Book(1, "How to Lose Friends and Alienate People", "Dale Carnegie"));
     }
 }
--- src/main/java/com/ifreann/spring/rest/simplerestcontroller/SimpleRestControllerApplication.java
+++ src/main/java/com/ifreann/spring/rest/simplerestcontroller/SimpleRestControllerApplication.java
@@ -2,12 +2,17 @@ package com.ifreann.spring.rest.simplerestcontroller;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.ApplicationContext;
+
+import java.util.Arrays;
 
 @SpringBootApplication
 public class SimpleRestControllerApplication {
 
-	public static void main(String[] args) {
-		SpringApplication.run(SimpleRestControllerApplication.class, args);
-	}
+    public static void main(String[] args) {
+
+        ApplicationContext applicationContext = SpringApplication.run(SimpleRestControllerApplication.class, args);
+        Arrays.stream(applicationContext.getBeanDefinitionNames()).forEach(bean -> System.out.println("got bean: " + bean));
+    }
 
 }
